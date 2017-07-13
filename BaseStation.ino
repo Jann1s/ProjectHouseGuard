909457550dc184f651e8af8016d5f1fc05da9b2a
A <0xaeternum@protonmail.com>
2017-07-13 12:36:46 +0200

    BaseStation FInal

--- BaseStation.ino
+++ BaseStation.ino
@@ -7,12 +7,25 @@
 //GLOBAL VARS
 //-----------
 
+//ANALOG PINS
+const int pinMQ = A0;         // MQ2 Sensor in KITCHEN
+
+//DIGITAL PINS
+const int pinPIR = 2;         // PIR Sensor in the LIVING ROOM
 
 char roomNumber = '0';        //Number (char) of the room where the alarm was triggered
 
 //---------
 //INSTANCES
 //---------
+
+// Variables for PIR 
+long interval = 2000000;          //Amount of time needed for the person to be considered idle (in milliseconds) 2MM = 0,56 hour
+unsigned long millisAtZero;       //Sets the milliseconds of the start timer
+unsigned long millisNow;          //Sets the milliseconds of the moment right now
+boolean timerSet = false;         //Indicates if the timer is activated or not
+boolean inRoom = false;           //Indicates if the person is in the room right now
+boolean itsPIR = false;           //Indicates if the PIR triggered the alarm or the MQ2 sensor
 
 //Creating BT instance
 SoftwareSerial BTserial(5, 6);    //TX --> 5 | RX --> 6
@@ -31,12 +44,15 @@ void setup()
 {  
   Serial.begin(9600);
  
+  pinMode(pinPIR, INPUT);               // Setting port for PIR
   SetupEthernet();                      // Setting up Ethernet Connection
   SetupBluetooth();                     // Setting up Bluetooth (Pairs automatically)
 }
  
 void loop()
 {
+  SmokeGasSensor();
+  PIRMotionSensor();
   reciveEvent();
 }
 
@@ -46,7 +62,53 @@ SENSORS
 ----------------------------------
 */
 
+//Smoke or gas detection (ANALOG)
+void SmokeGasSensor()         
+{
+  if (analogRead(pinMQ) > 400)
+  {
+    roomNumber = '1';
+    triggerEvent('f');      //sends alarmcode and roomnumber to the wristband
+    delay(5000);            //Delay to give the mq2 sensor some time to go back to the start value
+  }
+}
 
+//PIR Motion detection sensor (DIGITAL)
+void PIRMotionSensor()
+{
+  int sensorValue = digitalRead(pinPIR);  //Get the value of the PIR sensor (0 = NO MOTION or 1 = MOTION DETECTED)
+  
+  //True if the person is inside the room and idle - resets the millisAtZero and starts the timer
+  if (sensorValue == 0 && timerSet == false && inRoom == true)         
+  {
+    millisAtZero = millis();
+    timerSet = true;
+  }
+  //True if there is motion detected
+  if(sensorValue == 1)
+  {
+    inRoom == true;
+  }
+  //Cancels idelness and resets the timer
+  if (sensorValue == 1 && timerSet == true)
+  {
+    millisAtZero = millis();
+    timerSet = false;
+  }
+  
+  millisNow = millis();
+
+  //Timer interval reached
+  if(millisNow - millisAtZero > interval)
+  {
+    Serial.println("Idelness");
+    roomNumber = '0';
+    triggerEvent('p');    //p = trigger the problem alarm on the wristband
+    timerSet = false;     //stop the timer
+    itsPIR = true;        //let the basestation sms part know which sensor triggered the event
+    delay(5000);
+  }
+}
 
 /*
 ------------------------------------------------------
